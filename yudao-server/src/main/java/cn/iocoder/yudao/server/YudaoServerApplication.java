8a5875477bb97d3125b140b290ef00e655f720b5
wangxu <wangxu@powerlaw.ai>
2022-03-08 13:36:19 +0800

    本地项目启动

--- yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
 public class YudaoServerApplication {
 
+    // mvn clean package -Dmaven.test.skip=true   为什么要先执行这个？
     public static void main(String[] args) {
         SpringApplication.run(YudaoServerApplication.class, args);
     }
